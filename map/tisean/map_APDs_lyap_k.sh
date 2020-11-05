0256ae5d8827df8cc12d7cca016c4553c693a56f
cmarcotte3 <cmarcotte3@gatech.edu>
2020-11-05 12:38:41 -0500

    TISEAN updates

--- map/tisean/map_APDs_lyap_k.sh
+++ map/tisean/map_APDs_lyap_k.sh
@@ -1,8 +1,8 @@
-dir=/home/chris/Development/Julia/BR/map/
+dir=/home/chris/Development/Julia/BR/map/tisean
 
 for c in 1 2 3 4 5 6 7 8 9 10; do
-	for l in 50 100 500 1000 4000; do
-		./lyap_k -m 1 -M 3 -d 1 -R 5.0 -n 15 -s 10 -c $c -l 50 ${dir}/map_APDs.txt
+	for l in 10 20 30 40 50 100 500 1000 4000; do
+		./lyap_k -m 1 -M 8 -d 1 -R 5.0 -n 15 -s 10 -c $c -l $l ${dir}/map_APDs.txt
 		gnuplot -c ${dir}/tiseanplot.gp ${c} ${l} "${dir}/lyap_${c}_${l}.pdf"
 	done
 done
